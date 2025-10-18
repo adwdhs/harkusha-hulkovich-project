86ebc754ae6caf3fd564cfcb7ffb4c03dae588b4
Andrey <156023317+adwdhs@users.noreply.github.com>
2025-10-18 14:40:16 +0200

    Update settings.py

--- apiapp/settings.py
+++ apiapp/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://harkusha-hulkovich-api-production.up.railway.app",
+]
 # Application definition
 
 INSTALLED_APPS = [
