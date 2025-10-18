746567756136a7e8ad1ad4b8cd38973f3b0eff06
Vadzim Hulkovich <vadimgulkovich2000@gmail.com>
2025-10-18 13:44:05 +0200

    Add two new models (barcode and vatRate)

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -3,9 +3,9 @@ from .views import Index, Update, Add, Delete, Details
 
 
 urlpatterns = [
-    path("products", Index.as_view(), name="all"),
-    path("products/add", Add.as_view(), name="add"),
-    path("products/<str:pk>", Details.as_view(), name="details"),
-    path("products/<str:pk>/update", Update.as_view(), name="update"),
-    path("products/<str:pk>/delete", Delete.as_view(), name="delete"),
+    path("products", Index.as_view(), name="api_all"),
+    path("products/add", Add.as_view(), name="api_add"),
+    path("products/<str:pk>", Details.as_view(), name="api_details"),
+    path("products/<str:pk>/update", Update.as_view(), name="api_update"),
+    path("products/<str:pk>/delete", Delete.as_view(), name="api_delete"),
 ]
